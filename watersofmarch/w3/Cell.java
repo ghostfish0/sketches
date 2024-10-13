d70f6eb555172978e15d9216179dcb2d41e4911c
Tin Nguyen <nguyenvb.tin03@gmail.com>
2024-10-13 12:19:33 -0400

    cell and landscape implemented cavewall cells

--- watersofmarch/w3/Cell.java
+++ watersofmarch/w3/Cell.java
@@ -1,28 +1,42 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.geom.Arc2D
 
 public class Cell {
 
-	private boolean alive;
+	private int state;
+	//final private static int[] stateMap = {"left", "right", "up", "down"};
+    final private static int[] stateMap = {"◖", "◗", "⯊", "⯋"};
+	private static Map<String, Integer> reverseStateMap;
 
 	public Cell() { this(false); }
-
-	public Cell(boolean status) { this.alive = status; }
-
-	public boolean getAlive() { return this.alive; }
-
-	
-	public void setAlive(boolean status) { this.alive = status; }
-
-	
-	public void updateState(ArrayList<Cell> neighbors) {
-		int sum = neighbors.stream().mapToInt(i -> i.getAlive() ? 1 : 0).sum();
-		if (this.alive)
-			this.alive = (sum == 2 || sum == 3);
-		else
-			this.alive = (sum == 3);
+	public Cell(int state) {
+		this.alive = state;
+		for (int i = 0; i < stateMap.length; i++)
+			reverseStateMap.put(stateMap[i], i);
 	}
 
-	
-	public String toString() { return (this.alive ? "1" : "0"); }
-	public String toStringPretty() { return (this.alive ? "█" : "░"); }
+	public int getState() { return this.state; }
+
+	public void setState(int state) { this.state = state; }
+
+	public void updateState(ArrayList<Cell> neighbors) {}
+
+	public String toString() { return stateMap[this.state]; }
+
+	public static String toString(int state) { return stateMap[state]; }
+
+	public static int toState(String string) { return reverseStateMap.get(string); }
+
+	public void draw(Graphics g) {
+        //if (this.toString() == "left")
+        //    g.draw(Arc2D.Double(x, y, w, h, 90, 180, Arc2D.OPEN));
+        //else if (this.toString() == "right")
+        //    g.draw(Arc2D.Double(x, y, w, h, 90, -180, Arc2D.OPEN));
+        //else if (this.toString() == "up")
+        //    g.draw(Arc2D.Double(x, y, w, h, 0, -180, Arc2D.OPEN));
+        //else if (this.toString() == "down") 
+        //    g.draw(Arc2D.Double(x, y, w, h, 0, -180, Arc2D.OPEN));
+	}
 }
--- watersofmarch/w3/Landscape.java
+++ watersofmarch/w3/Landscape.java
@@ -4,32 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Random;
 
 public class Landscape {
 
 	
 	private Cell[][] landscape;
-	private Queue<Cell[][]> historicalLandscapes;
-	private Random rand;
+	final static private Random rand = new Random();
 
-	private double initialChance;
-
-	public Landscape(int rows, int columns) { this(rows, columns, 0); }
-
-	public Landscape(int rows, int cols, double chance) {
-		this.rand = new Random();
+	public Landscape(int rows, int cols) {
 		this.landscape = new Cell[rows][cols];
-		this.historicalLandscapes = new LinkedList<>();
-		for (int iterator = 0; iterator < 20; iterator++) {
-			historicalLandscapes.add(landscape);
-		}
-		this.initialChance = chance;
 		reset();
 	}
 
 	public Landscape(boolean[][] grid) {
-		this(grid.length, grid[0].length, 0);
 		int rows = grid.length;
 		int cols = grid[0].length;
 		for (int row = 0; row < rows; row++) {
@@ -39,25 +26,12 @@ public class Landscape {
 		}
 	}
 
-	
-	public Landscape(String[] grid) {
-		this(grid.length, grid[0].length(), 0);
-		int rows = grid.length;
-		int cols = grid[0].length();
-		for (int row = 0; row < rows; row++) {
-			for (int col = 0; col < cols; col++) {
-				this.landscape[row][col] = new Cell(grid[row].charAt(col) == 'x');
-			}
-		}
-	}
-
 	public void reset() {
 		int rows = this.landscape.length;
 		int cols = this.landscape[0].length;
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
-				this.landscape[row][col] =
-				        new Cell(this.rand.nextDouble() < this.initialChance);
+				this.landscape[row][col] = new Cell(this.rand.nextInt(4));
 			}
 		}
 	}
@@ -74,7 +48,7 @@ public class Landscape {
 		int cols = this.landscape[0].length;
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
-				str += this.landscape[row][col].toStringPretty();
+				str += this.landscape[row][col].toString();
 			}
 			str += "\n";
 		}
@@ -105,21 +79,16 @@ public class Landscape {
 		// init the temporary, editable landscape
 		Cell[][] workLandscape = new Cell[rows][cols];
 
-		// remove the oldest historicalLandscape from the head of the queue
-		this.historicalLandscapes.poll();
-		// add the latest (current) landscape to the tail of the queue
-		this.historicalLandscapes.offer(this.landscape);
-
 		// creates a copy of the currentLandscape;
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
-				workLandscape[row][col] = new Cell(this.landscape[row][col].getAlive());
+				workLandscape[row][col] = new Cell(this.landscape[row][col].getState());
 			}
 		}
 		// update the temporary work landscape
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
-				workLandscape[row][col].updateState(getNeighbors(row, col));
+				workLandscape[row][col].updateState();
 			}
 		}
 		// update the current landscape
@@ -127,42 +96,12 @@ public class Landscape {
 	}
 
 	
-	public void setCellAlive(double x, double y) {
-		int row = (int)Math.floor(x * getRows());
-		int col = (int)Math.floor(y * getCols());
-		if (row < 0 || row >= getRows() || col < 0 || col > getCols())
-			return;
-		this.landscape[row][col].setAlive(true);
-	}
-
-	public int getSum() {
-		return Arrays.stream(this.landscape).flatMap(Arrays::stream).mapToInt(i -> i.getAlive() ? 1 : 0).sum();
-	}
-
-	public void drawScape(Graphics g, int scale, Cell[][] scape, Color color) {
+	public void drawScape(Graphics g, int scale, Color color) {
 		g.setColor(color);
-		for (int x = 0; x < getRows(); x++) {
-			for (int y = 0; y < getCols(); y++) {
-				if (scape[x][y].getAlive()) {
-					g.fillRect(x * scale, y * scale, scale, scale);
-				}
-			}
-		}
+        g.drawString("" + this, 0, 0);
 	}
 
 	public void draw(Graphics g, int scale) {
-		int redness = 0;
-		// Copy of historicalLandscapes to avoid For loop modification error
-		ArrayList<Cell[][]> historicalLandscapesCopy = new ArrayList<>(historicalLandscapes);
-		for (Cell[][] scape : historicalLandscapesCopy) {
-			if (scape[0][0] == null)
-				continue;
-			redness = (redness + 10 < 255 ? redness + 10 : 255);
-			Color color = new Color(redness, 0, 0);
-			drawScape(g, scale, scape, color);
-		}
-		drawScape(g, scale, this.landscape, Color.WHITE);
+		drawScape(g, scale, Color.WHITE);
 	}
-
-	public static void main(String[] args) {}
 }
