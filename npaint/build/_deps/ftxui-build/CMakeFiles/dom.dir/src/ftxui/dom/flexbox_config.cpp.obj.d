14d6a9c69c05affc95c5b851616a09dd8b73962a
Tin Nguyen <nguyenvb.tin03@gmail.com>
2024-12-06 07:29:49 +0700

    gitignore

--- npaint/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj: \
- C:\Users\tinnguyen\Documents\notes\gamedev\sketches\npaint\build\_deps\ftxui-src\src\ftxui\dom\flexbox_config.cpp \
- C:/Users/tinnguyen/Documents/notes/gamedev/sketches/npaint/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
