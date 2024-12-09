9be26cdd3c55a442a848d1c30cf1d34e8e63510b
Tin Nguyen <nguyenvb.tin03@gmail.com>
2024-12-09 13:00:25 -0500

    main window started working

--- npaint/src/main.cpp
+++ npaint/src/main.cpp
@@ -16,89 +16,97 @@
 #include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
 #include "ftxui/dom/canvas.hpp"			  // for Canvas
 #include "ftxui/screen/color.hpp"		  // for Color, Color::Red, Color::Blue, Color::Green, ftxui
+	//
+
+using namespace ftxui;
+
+Element
+ColorTile(int red, int green, int blue)
+{
+	return text("") | size(WIDTH, GREATER_THAN, 14) | size(HEIGHT, GREATER_THAN, 7) | bgcolor(Color::RGB(red, green, blue));
+}
+
+Element
+ColorString(int red, int green, int blue)
+{
+	return text("RGB = (" +			  //
+		    std::to_string(red) + "," +	  //
+		    std::to_string(green) + "," + //
+		    std::to_string(blue) + ")"	  //
+	);
+}
 
 int
 main()
 {
-	using namespace ftxui;
-
 	int mouse_x = 0;
 	int mouse_y = 0;
 
-	auto renderer_plot_3 = Renderer([&] {
-		auto c = Canvas(200, 100);
-		c.DrawText(0, 0, "A 2D gaussian plot");
-		int size = 20;
+	int red = 128;
+	int green = 25;
+	int blue = 100;
+	auto slider_red = Slider("Red  :", &red, 0, 255, 1);
+	auto slider_green = Slider("Green:", &green, 0, 255, 1);
+	auto slider_blue = Slider("Blue :", &blue, 0, 255, 1);
 
-		// mouse_x = 5mx + 3*my
-		// mouse_y = 0mx + -5my + 90
-		// this part is alpine transforming the mouse coordinate into 3d
-        float scalek = 7.f;
-        float scalej = 3.f;
-		float my = (mouse_y - 90) / -scalek;
-		float mx = (mouse_x - scalej * my) / scalek;
-		std::vector<std::vector<float>> ys(size, std::vector<float>(size));
-		for (int y = 0; y < size; y++) {
-			for (int x = 0; x < size; x++) {
-				float dx = x - mx;
-				float dy = y - my;
-				ys[y][x] = -scalej / 2 + scalej * std::exp(-1/scalek * (dx * dx + dy * dy));
-			}
-		}
-		for (int y = 0; y < size; y++) {
-			for (int x = 0; x < size; x++) {
-				if (x != 0) {
-					c.DrawPointLine(scalek * (x - 1) + scalej * (y - 0),
-							90 -scalek * (y - 0) - 5 * ys[y][x - 1],
-							scalek * (x - 0) + scalej * (y - 0),
-							90 - scalek * (y - 0) - scalek * ys[y][x]);
-				}
-				if (y != 0) {
-					c.DrawPointLine(scalek * (x - 0) + scalej * (y - 1),
-							90 - scalek * (y - 1) - scalek * ys[y - 1][x],
-							scalek * (x - 0) + scalej * (y - 0),
-							90 - scalek * (y - 0) - scalek * ys[y][x]);
-				}
-			}
-		}
+	auto sidebar = Container::Vertical({
+		slider_red,
+		slider_green,
+		slider_blue,
+	});
 
-		return canvas(std::move(c));
+	auto sidebar_renderer = Renderer(sidebar, [&] {
+		return vbox({
+			ColorTile(red, green, blue),
+			separator(),
+			slider_red->Render(),
+			separator(),
+			slider_green->Render(),
+			separator(),
+			slider_blue->Render(),
+			separator(),
+			ColorString(red, green, blue),
+		});
+	});
+
+	auto renderer_line_block = Renderer([&] {
+		auto c = Canvas(100, 100);
+		c.DrawText(0, 0, "Several lines (block)");
+		c.DrawBlockLine(mouse_x, mouse_y, 80, 10, Color::Red);
+		c.DrawBlockLine(80, 10, 80, 40, Color::Blue);
+		c.DrawBlockLine(80, 40, mouse_x, mouse_y, Color::Green);
+		return hbox({ canvas(std::move(c)) | borderDashed }) | center;
 	});
 
 	int selected_tab = 0;
 	auto tab = Container::Tab(
 		{
-			renderer_plot_3,
+			renderer_line_block,
 		},
 		&selected_tab);
 
 	// This capture the last mouse position.
 	auto tab_with_mouse = tab | CatchEvent([&](Event e) {
-		if (e.is_mouse()) {
-			mouse_x = (e.mouse().x - 1) * 2;
-			mouse_y = (e.mouse().y - 1) * 4;
-		}
-		return false;
-	});
+				      if (e.is_mouse()) {
+					      mouse_x = (e.mouse().x - 1) * 2;
+					      mouse_y = (e.mouse().y - 1) * 4;
+				      }
+				      return false;
+			      });
 
-	std::vector<std::string> tab_titles = {
-		"plot_3 3D",
-	};
-	auto tab_toggle = Menu(&tab_titles, &selected_tab);
-
-    // the container 
+	// the container
 	auto component = Container::Horizontal({
+		sidebar,
 		tab_with_mouse,
-		tab_toggle,
 	});
 
-    // Tell (modify) the container how to display its content
+	// Tell (modify) the container how to display its content
 	// Add some separator to decorate the whole component:
 	auto component_renderer = Renderer(component, [&] {
 		return hbox({
-			       tab_with_mouse->Render() | border,
+			       sidebar_renderer->Render() | size(WIDTH, LESS_THAN, 80),
 			       separator(),
-			       tab_toggle->Render(),
+			       tab_with_mouse->Render() | xflex,
 		       }) |
 		       border;
 	});
