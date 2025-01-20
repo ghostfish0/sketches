ccaf2d7bb25fd24b52d7579c83716d8759994d54
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-01-19 21:41:45 -0500

    gridpainting

--- Gridpainting/Gridpainting.java
+++ Gridpainting/Gridpainting.java
@@ -1,16 +1,25 @@
+import controlP5.*;
 import processing.core.PApplet;
 import processing.event.MouseEvent;
 
 public class Gridpainting extends PApplet {
+
 	final private int width = 500;
 	final private int height = 500;
 	final private int rows = 20;
+
 	final private int column = 20;
+	// 0: NORMAL
+	// 1: MULTI-NORMAL
+	// 2: BRUSH
+    final private int mode = 0;
+    private ControlP5 gui;
 
 	private int lagger = 0;
 	private int laggerLimit = 5;
 
-    private int wheelCnt = 0;
+	private int wheelCnt = 0;
+
 	private int brushState = 1;
 
 	private Grid grid = new Grid(rows, column, width, height, this);
@@ -20,6 +29,7 @@ public class Gridpainting extends PApplet {
 	public void settings() { size(grid.getWidth(), grid.getHeight()); }
 
 	public void setup() {
+		gui = new ControlP5(this);
 		// frameRate(5);
 		background(200);
 		grid.reset();
@@ -28,29 +38,25 @@ public class Gridpainting extends PApplet {
 
 	public void draw() {
 		background(200);
-        grid.updateCursor();
+		grid.updateCursor();
 		grid.draw();
-		//if ((lagger++) % laggerLimit == 0)
+		// if ((lagger++) % laggerLimit == 0)
 		//	grid.advance();
 	}
 
-	public void mouseDragged() {
-        grid.paintCellAtCursor();
-	}
+	public void mouseDragged() { grid.paintCellAtCursor(); }
 
 	public void mouseClicked() { mouseDragged(); }
 
-	public void keyTyped() {
-	}
-	public void keyReleased() {
-	}
+	public void keyTyped() {}
+	public void keyReleased() {}
 
-	//public void mouseWheel(MouseEvent event) {
-	//       wheelCnt++;
-	//	if (wheelCnt % 10 == 0) {
-	//		brushState++;
-	//		brushState %= brushLimit;
-	//		brush.setState(brushState);
-	//	}
-	//}
+	public void mouseWheel(MouseEvent event) {
+		// if (wheelCnt % 10 == 0) {
+		//       wheelCnt++;
+		//	brushState++;
+		//	brushState %= brushLimit;
+		//	brush.setState(brushState);
+		//}
+	}
 }
---
+++ Gridpainting/journal.md
@@ -0,0 +1,10 @@
+# Gridpainting Journal 
+
+## log date 2024-12-13
+Essentially a way for me to conceptually, and temporally think and revise my organization of this code 
+
+I want to make a pixel editor heavily inspired by vim and all around just comfort with the keyboard when editing pixel art
+
+- Features to implement
+    - 
+
